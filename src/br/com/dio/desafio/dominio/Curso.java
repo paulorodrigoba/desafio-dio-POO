a5255f6323fbddf87a0ad7d0fd24080307e30935
Paulo Rodrigo <paulorodrigoba@outlook.com>
2024-09-25 01:11:16 -0400

    atualização da classe

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,4 +1,33 @@
 package br.com.dio.desafio.dominio;
 
 public class Curso {
+    private String curso;
+    private String descricao;
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "curso='" + curso + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+
+    public Curso() {
+    }
 }
